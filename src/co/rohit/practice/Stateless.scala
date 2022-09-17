05ba07eb22802916070cc6b3a95caf47de2a16ce
Rohit Shingare <rohit_shingare@persistent.co.in>
2022-09-17 16:24:51 +0530

    Stateless code added

--- src/co/rohit/practice/Stateless.scala
+++ src/co/rohit/practice/Stateless.scala
@@ -1,5 +1,35 @@
 package co.rohit.practice
-
-object Stateless {
+import org.apache.spark.SparkConf
+import org.apache.spark._
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.Seconds
+import org.apache.spark.SparkContext
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+object Stateless extends App{
+  
+   
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  
+  val sc = new SparkConf()
+  sc.set("spark.app.name", "stateless")
+  sc.set("spark.master", "local[*]")
+  
+  val ssc = new StreamingContext(sc,Seconds(5))
+  
+  val lines = ssc.socketTextStream("localhost",9998)
+  
+  val worlds = lines.flatMap(x => x.split(" "))
+  
+  val pairs = worlds.map(x => (x,1))
+  
+  val wordCounts = pairs.reduceByKey((x,y) => x+y)
+  
+  wordCounts.print()
+  
+  ssc.start()
+  
+  ssc.awaitTermination()
+  
   
 }
